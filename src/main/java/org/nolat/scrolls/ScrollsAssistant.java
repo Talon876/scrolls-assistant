9258218b500dcf43c0541704b1d7ae9ce05be8fc
Talon Daniels <talondaniels@gmail.com>
2013-10-05 15:50:13 -0600

    Switch logging framework and implement encryption
    * Switched from slf4j to log4j
    * Implemented encryption for logging in to Scrolls

--- src/main/java/org/nolat/scrolls/ScrollsAssistant.java
+++ src/main/java/org/nolat/scrolls/ScrollsAssistant.java
@@ -1,11 +1,25 @@
 package org.nolat.scrolls;
 
+import java.util.Date;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public class ScrollsAssistant {
+
+    private static final Logger log = Logger.getLogger(ScrollsAssistant.class);
+
     public ScrollsAssistant() {
 
     }
 
+    /**
+     * Entry point for the Scrolls Assistant
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Scrolls Assistant");
+        BasicConfigurator.configure();
+        log.info("Starting " + ScrollsAssistant.class.getSimpleName() + " " + (new Date()).toString());
     }
 }
