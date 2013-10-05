2042699658c7b788a64f60513ec4de54d59cd960
Talon Daniels <talondaniels@gmail.com>
2013-10-05 14:27:42 -0600

    Added initial code and gradle build system

---
+++ src/main/java/org/nolat/scrolls/ScrollsAssistant.java
@@ -0,0 +1,9 @@
+public class ScrollsAssistant {
+	public ScrollsAssistant() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Scrolls Assistant");
+	}
+}
---
+++ src/test/java/org/nolat/scrolls/ExampleTest.java
@@ -0,0 +1,14 @@
+package org.nolat.scrolls;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test public void testTest() {
+        assertTrue(1 == 1);
+    }
+
+    @Test public void testTest2() {
+        assertFalse(1 == 2);
+    }
+}
